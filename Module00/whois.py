190501450107023dcb153c2723e691d3f5a4b086
Yassine-Zaazaa <yassine.zaazaa@um6p.ma>
2021-04-08 21:36:25 +0100

    Exercise 2

--- Module00/whois.py
+++ Module00/whois.py
@@ -1,16 +1,18 @@
 import sys
 
 def even_odd_zero(number):
-    if number % 2 == 0:
-        print("I'm Even.")
-    elif number == 0:
-        print("I'm Zero.")
-    else:
-        print("I'm Odd.")
-  
+    if number.isdigit():
+        number = int(number)
+        if number == 0:
+            return "I'm Zero."
+        elif number % 2 == 0:
+            return "I'm Even."
+        else:
+            return "I'm Odd."
+    return "ERROR"
     
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 2 or len(sys.argv) == 1 :
         print("ERROR")
     else:
-        even_odd_zero(int(sys.argv[1]))     
+        print(even_odd_zero(sys.argv[1]))     
